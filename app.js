625eb6c2a54f8b32e5f77b41b8358ffb66429c58
Ruby Li <r844312@gmail.com>
2021-05-08 14:50:43 +0800

    heroku init

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express')
 const app = express()
 
 
-app.listen('3000', () => {
+app.listen(process.env.PORT || '3000', () => {
   console.log('app is listening on port 3000')
 })
 
