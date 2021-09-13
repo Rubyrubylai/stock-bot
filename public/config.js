dbae37c0f83711b911668ec01b773304d43972b7
Ruby Li <r844312@gmail.com>
2021-09-13 22:45:27 +0800

    Update config

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 const config = {
   follow_liffId : '1656012903-kmXAo1L4',
   list_liffId : '1656012903-zw9NvQP6',
-  domain: 'http://localhost:3000'
+  domain: 'https://lit-bastion-68018.herokuapp.com'
 }
 
 export default config
